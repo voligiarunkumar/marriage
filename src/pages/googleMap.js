e63971454422d3ce7550855c5b755de11fdac58d
unknown <voligiarunkumar9912345@gmail.com>
2023-04-17 01:30:10 +0530

    addedlctin

--- src/pages/googleMap.js
+++ src/pages/googleMap.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import "../css/google.css"
-import MAP from '../picture/MAP.png'
-function GoogleMap(){
+import React from "react";
+import "../css/google.css";
+import MAP from "../picture/MAP.png";
+function GoogleMap() {
+  let googleMaps = (
+    <div className="googleMap">
+      <iframe
+        title="myFrame"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15665.117830082085!2d76.126683!3d11.017647!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa06c470ad135702d!2sHeavens%20Convention%20center!5e0!3m2!1sen!2sin!4v1577719067341!5m2!1sen!2sin"
+        style={{
+          width: "500px",
+          height: "250px",
+          frameborder: 0,
+          border: 0,
+          allowfullscreen: "",
+        }}
+      ></iframe>
+    </div>
+  );
 
-let googleMaps=(<div className="googleMap">
+  
 
-
-<iframe  title="myFrame" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15665.117830082085!2d76.126683!3d11.017647!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa06c470ad135702d!2sHeavens%20Convention%20center!5e0!3m2!1sen!2sin!4v1577719067341!5m2!1sen!2sin" 
-style={{width :"500px",height:"250px", frameborder:0,border:0,allowfullscreen:""}}  ></iframe>
-</div>)
-
-
-let googleMapss=(<div className="googleMapss">
-
-
-<iframe  title="myFrames" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15665.117830082085!2d76.126683!3d11.017647!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa06c470ad135702d!2sHeavens%20Convention%20center!5e0!3m2!1sen!2sin!4v1577719067341!5m2!1sen!2sin"
-style={{width :"250px",height:"250x", frameborder:0,border:0,allowfullscreen:""}}  ></iframe>
-
-</div>)
-
-
-let Narkkilakkad=
-(<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3897.79809809599!2d75.31401744950838!3d12.329372291236682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba460c0ea5cc909%3A0xd585d024e4bb61f4!2sMalankara%20Church!5e0!3m2!1sen!2sin!4v1648304851324!5m2!1sen!2sin"
-style={{width:"100",height:"100",border:0}}
- allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>)
-return (
-<div>
-{Narkkilakkad}
-</div>)
-
+  let Narkkilakkad = (
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3813.4993126127756!2d78.0277987741169!3d17.09712651110416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbd140828a58d7%3A0x40b8b756413b3d5f!2sGM%20PATEL%20GARDENS!5e0!3m2!1sen!2sin!4v1681674440701!5m2!1sen!2sin"
+        width="600"
+        height="450"
+        style={{width:'300px',height:'70px',border:0}}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+  );
+  return <div>{Narkkilakkad}</div>;
 }
 
-export default GoogleMap
+export default GoogleMap;
